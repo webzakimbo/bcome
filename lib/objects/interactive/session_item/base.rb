65cdb037d19027d98cf7d8d3a7af5202d117b1b9
webzakimbo <guillaume@webzakimbo.com>
2021-03-11 09:39:15 +0000

    as before - no need for newline in interactive mode's Reline input grab

--- lib/objects/interactive/session_item/base.rb
+++ lib/objects/interactive/session_item/base.rb
@@ -32,7 +32,7 @@ module Bcome::Interactive::SessionItem
     end
 
     def get_input(message = terminal_prompt)
-      ::Reline.readline("\n#{message}", true).squeeze('').to_s
+      ::Reline.readline("#{message}", true).squeeze('').to_s
     end
   end
 end
