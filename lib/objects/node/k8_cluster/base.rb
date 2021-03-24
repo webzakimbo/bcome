2d5dcff45f412f6d42123490d7cc944c909345ff
guillaume <guillaume.roderick@gfk.com>
2021-03-24 10:43:15 +0000

    1. Can now 'get' arbitrary CRDs, 2. IRB echo back on... useful for debugging

--- lib/objects/node/k8_cluster/base.rb
+++ lib/objects/node/k8_cluster/base.rb
@@ -7,5 +7,11 @@ module Bcome::Node::K8Cluster
       "#{parent.kubectl_context}.#{hyphenated_identifier}"
     end
 
+    def get(crd_key)
+      data = run_kc("get #{crd_key}")
+      raise ::Bcome::Exception::Generic, "No items returned from call to 'get #{crd_key}'" if !data.is_a?(Hash) && data.has_key?(:items)
+      return data["items"]
+    end
+
   end
 end
